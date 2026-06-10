5cbda522d13b43353291784441a0147eb4ac5ccf
Dyllom <demon1309w@gmail.com>
2026-06-10 20:26:36 -0300

    Remove comments and clean up main.py

--- Kiosco_Prueba/main.py
+++ Kiosco_Prueba/main.py
@@ -2,9 +2,6 @@ import database as db
 from gui import KioscoApp
 
 if __name__ == "__main__":
-    # Inicializa las tablas con la carga masiva de productos
     db.init_db()
-    
-    # Arranca el bucle gráfico principal
     app = KioscoApp()
     app.mainloop()
